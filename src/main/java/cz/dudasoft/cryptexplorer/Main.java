acd8deb876d04b76f929f1c3ff1965b424eb79ea
Jan Duda <doodah737@gmail.com>
2025-01-03 17:56:58 +0100

    Basic structure

---
+++ src/main/java/cz/dudasoft/cryptexplorer/Main.java
@@ -0,0 +1,8 @@
+package cz.dudasoft.cryptexplorer;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Vítejte v Crypt Explorer!");
+        System.out.println("Připravte se na dobrodružství...");
+    }
+}
