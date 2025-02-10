703b411c08132e526e120799e929f9c2fc84a7a2
Jan Duda <doodah737@gmail.com>
2025-02-10 19:35:00 +0100

    Allow healing when staying on the same field

--- src/main/java/cz/dudasoft/cryptexplorer/character/Character.java
+++ src/main/java/cz/dudasoft/cryptexplorer/character/Character.java
@@ -6,10 +6,12 @@ import cz.dudasoft.cryptexplorer.item.Weapon;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public abstract class Character {
     private final String name;
     private int health;
+    private int maxHealth;
     private final List<Item> inventory;
     private Weapon equippedWeapon;
     private Armor equippedArmor;
@@ -19,6 +21,7 @@ public abstract class Character {
     public Character(String name, int health) {
         this.name = name;
         this.health = health;
+        this.maxHealth = health;
         this.inventory = new ArrayList<>();
     }
 
@@ -39,7 +42,7 @@ public abstract class Character {
     }
 
     public void heal(int amount) {
-        this.health += amount;
+        this.health = Math.min(health + amount, maxHealth);
     }
 
     public List<Item> getInventory() {
@@ -81,10 +84,24 @@ public abstract class Character {
         return y;
     }
 
-    public void updatePosition(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public void updatePosition(int newX, int newY) {
+        if (this.x == newX && this.y == newY) {
+            attemptHealing();
+        } else {
+            this.x = newX;
+            this.y = newY;
+        }
     }
+
+    private void attemptHealing() {
+        Random random = new Random();
+        if (random.nextDouble() < 0.2) { // 10% chance to heal
+            int healAmount = 1 + random.nextInt(2); // Heal 1-2 HP
+            heal(healAmount);
+            System.out.println("You take a moment to rest and heal " + healAmount + " HP.");
+        }
+    }
+
 
     public abstract void attack(Character target);
 }
